ba5c36e9c52d4add63dcbeaf6af27cc413c1b227
qurious-pixel <62252937+qurious-pixel@users.noreply.github.com>
2023-07-30 20:09:05 -0700

    Create appimage.sh

---
+++ linux/appimage/appimage.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+curl -SL https://github.com/AppImage/AppImageKit/releases/download/continuous/appimagetool-x86_64.AppImage -o appimagetool
+
+mkdir -p AppDir/usr/
+cp -r linux/releasex64 AppDir/usr/bin
+chmod +x AppDir/usr/bin
+ln -sr AppDir/usr/bin/quake2 AppDir/AppRun
+
+cp linux/appimage/vkQuake2_512x512.png AppDir/vkquake.png
+cp linux/appimage/vkquake.desktop AppDir/vkquake.desktop
+mkdir -p AppDir/usr/share/applications && cp ./AppDir/vkquake.desktop ./AppDir/usr/share/applications
+mkdir -p AppDir/usr/share/icons && cp ./AppDir/vkquake.png ./AppDir/usr/share/icons
+mkdir -p AppDir/usr/share/icons/hicolor/512x512/apps && cp ./AppDir/vkquake.png ./AppDir/usr/share/icons/hicolor/512x512/apps
+
+chmod a+x appimagetool
+./appimagetool AppDir/ vkQuake.AppImage
